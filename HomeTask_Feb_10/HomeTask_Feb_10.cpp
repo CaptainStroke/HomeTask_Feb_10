6be674d7a800c477a3da2aa99d4e864a26815838
Glib Petrachkov <petrasha@gmail.com>
2022-02-12 10:44:24 +0200

    problem 3. Ok done

--- HomeTask_Feb_10/HomeTask_Feb_10.cpp
+++ HomeTask_Feb_10/HomeTask_Feb_10.cpp
@@ -36,76 +36,17 @@
 //2. Даны 2 массива размерности M и N соответственно. Необходимо переписать в третий массив общие элементы
 //первых двух массивов, без повторений.
 
-#include <iostream>
-#include <algorithm>
-using namespace std;
-int main()
-{
-	const int SIZE = 50;
-	int ar[SIZE];
-	int br[SIZE];
-	int j = 0;
-	int l = 0;
-	int cr[SIZE];
-	cout << "Original array M\n";
-	for (int i = 0; i < SIZE; i++)
-	{
-		ar[i] = rand() % 100;
-		cout << ar[i] << ", ";
-	}
-	cout << "\nOriginal array N\n";
-	for (int i = 0; i < SIZE; i++)
-	{
-		br[i] = rand() % 100;
-		cout << br[i] << ", ";
-	}
-	cout << "\nCombined in one array Z\n";
-	for (int i = 0; i < SIZE; i++)
-	{
-		for (int j = 0; j < SIZE; j++)
-		{
-			if (ar[i] == br[j]) cr[i] = ar[i];
-		}
-		cout << cr[i] << ", ";
-	}
-	cout << "\n Doubles sorted\n";
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (cr[i] < -2 || cr[i] > 101) cr[i] = 101;
-		cout << cr[i] << ", ";
-	}
-	    cout << "\nArray groomed\n";
-		sort(cr, cr + SIZE);
-		for (int i = 0; i < SIZE; i++)
-	{
-		if (cr[i] == cr[i - 1]) cr[i] = 101; 
-		cout << cr[i] << ", ";
-	}
-		cout << "\n Final touch\n";
-		sort(cr, cr + SIZE);
-		for (int i = 0; i < SIZE; i++)
-		{
-			if (cr[i] < -2 || cr[i] >= 101) cr[i] = -1;
-			cout << cr[i] << ", ";
-		}
-		cout << "\n";
-}
-
-//3. Даны 2 массива размерности M и N соответственно.Необходимо переписать в третий массив те элементы
-//первого массива, которых нет во втором массиве, без повторений.
-
 //#include <iostream>
 //#include <algorithm>
 //using namespace std;
 //int main()
 //{
 //	const int SIZE = 50;
-//	const int KING = 100;
 //	int ar[SIZE];
 //	int br[SIZE];
 //	int j = 0;
 //	int l = 0;
-//	int cr[KING];
+//	int cr[SIZE];
 //	cout << "Original array M\n";
 //	for (int i = 0; i < SIZE; i++)
 //	{
@@ -119,33 +60,105 @@ int main()
 //		cout << br[i] << ", ";
 //	}
 //	cout << "\nCombined in one array Z\n";
-//	for (int i = 0; i < KING; i++)
+//	for (int i = 0; i < SIZE; i++)
 //	{
-//		if (i % 2 == 0) cr[i] = ar[j], j++;
-//		if (i % 2 != 0) cr[i] = br[l], l++;
+//		for (int j = 0; j < SIZE; j++)
+//		{
+//			if (ar[i] == br[j]) cr[i] = ar[i];
+//		}
 //		cout << cr[i] << ", ";
 //	}
-//	cout << "\nSorted\n";
-//	sort(cr, cr + KING);
-//	for (int i = 0; i < KING; i++)
+//	cout << "\n Doubles sorted\n";
+//	for (int i = 0; i < SIZE; i++)
 //	{
+//		if (cr[i] < -2 || cr[i] > 101) cr[i] = 101;
 //		cout << cr[i] << ", ";
 //	}
-//	cout << "\nCopies turned in \"  100s\" \n";
-//	for (int i = 0; i < KING; i++)
+//	    cout << "\nArray groomed\n";
+//		sort(cr, cr + SIZE);
+//		for (int i = 0; i < SIZE; i++)
 //	{
-//		if (cr[i] == cr[i - 1]) cr[i] = 100;
+//		if (cr[i] == cr[i - 1]) cr[i] = 101; 
 //		cout << cr[i] << ", ";
 //	}
-//	cout << "\nCopies removed to end and turned to \" -1\"\n";
-//	sort(cr, cr + KING);
-//	for (int i = 0; i < KING; i++)
-//	{
-//		if (cr[i] == 100) cr[i] = -1;
-//		cout << cr[i] << ", ";
-//	}
-//	cout << "\n";
+//		cout << "\n Final touch\n";
+//		sort(cr, cr + SIZE);
+//		for (int i = 0; i < SIZE; i++)
+//		{
+//			if (cr[i] < -2 || cr[i] >= 101) cr[i] = -1;
+//			cout << cr[i] << ", ";
+//		}
+//		cout << "\n";
 //}
+
+//3. Даны 2 массива размерности M и N соответственно.Необходимо переписать в третий массив те элементы
+//первого массива, которых нет во втором массиве, без повторений.
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	const int SIZE = 50;
+	const int KING = 100;
+	int ar[SIZE];
+	int br[SIZE];
+	int i = 0;
+	int j = 0;
+	int l = 0;
+	int cr[SIZE];
+	cout << "Original array M\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		ar[i] = rand() % 100;
+		cout << ar[i] << ", ";
+	}
+	cout << "\nOriginal N\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		br[i] = rand() % 100;
+		cout << br[i] << ", ";
+	}
+	sort(ar, ar + SIZE);
+	cout << "\nSorted array M\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << ar[i] << ", ";
+	}
+	sort(br, br + SIZE);
+	cout << "\nSorted array N\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << br[i] << ", ";
+	}
+
+	cout << "\nCombined in one array Z\n";
+	for (int i = 0; i < SIZE; i++)
+			for (int j = 0; j < SIZE; j++)
+		{
+				if (ar[i] == br[j]) 
+				{
+					cr[i] = 101;
+					break;
+				}
+				else if (ar[i] != br[j]) cr[i] = ar[i];
+		}
+	cout << "\nRemove repeats\n";
+		sort(cr, cr + SIZE);
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (cr[i] == cr[i - 1]) cr[i] = 101;
+		}
+
+		sort(cr, cr + SIZE);
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (cr[i] == 101) cr[i] = -1;
+			cout << cr[i] << ", ";
+		}
+		cout << "\nFinal\n";
+}
+
 
 //4. Реализовать программу - лотерею. Программа загадывает 5 случайных неповторяющихся чисел в диапазоне
 //от 1 до 42, но не показывает их на экран.Пользователь пытается их угадать – вводит какие - то свои 5 чисел
